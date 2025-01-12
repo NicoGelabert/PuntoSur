6c0958d735ec4dd8765d47caf08b5f15d7f41de6
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-01-12 13:37:13 +0100

    Pst footer & Favicon

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -25,25 +25,6 @@
     </div>
     <hr class="footer-divider" />
     <div class="post-footer">
-        <span class="developed-by">{{__('Sitio diseñado y desarrollado por')}}<a href="https://nicolasgelabert.com.ar" target="_blank"> Nicolás Gelabert</a></span>
-        <ul class="flex gap-x-4">
-            @foreach (Config::get('languages') as $lang => $language)
-                @if ($lang != App::getLocale())
-                    <li>
-                        <a class="flex items-center gap-x-1 opacity-50" href="{{ route('lang.switch', $lang) }}">
-                            <span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span>
-                            <span>{{$language['display']}}</span>
-                        </a>
-                    </li>
-                @else
-                    <li>
-                        <a class="flex items-center gap-x-1" href="{{ route('lang.switch', $lang) }}">
-                            <span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span>
-                            <span>{{$language['display']}}</span>
-                        </a>
-                    </li>
-                @endif
-            @endforeach
-        </ul>
+        <span class="developed-by">Site designed and developed by <a href="https://chimicreativo.es" target="_blank">CHIMI CREATIVO</a></span>
     </div>
 </footer>
