99ecd8c918920e403912a3bb2bd85c23211bf336
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-02-05 17:37:02 +0100

    icons footer

--- resources/views/components/social-icons.blade.php
+++ resources/views/components/social-icons.blade.php
@@ -1,11 +1,11 @@
 <div  {{ $attributes->merge(['class' => 'flex gap-4 social-icons']) }}>
-    <a href="https://wa.me/34623037048" target="_blank">
-        <x-icons.phone />
+    <a href="https://wa.me/353852727422" target="_blank">
+        <x-icons.whatsapp />
     </a>
-    <a href="https://www.instagram.com/chimi.creativo/" target="_blank">
-        <x-icons.mail />
+    <a href="https://www.facebook.com/Chibalancetherapies" target="_blank">
+        <x-icons.facebook />
     </a>
-    <a href="https://www.instagram.com/chimi.creativo/" target="_blank">
+    <a href="https://maps.app.goo.gl/wAKdYQRLxSdrLY4U6" target="_blank">
         <x-icons.map />
     </a>
 </div>
