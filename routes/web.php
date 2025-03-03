2ac058878c5a82d7fa7ac11fe9f8a20cb6c1c0d0
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-03-03 12:58:04 +0100

    Web publishing

--- routes/web.php
+++ routes/web.php
@@ -36,11 +36,11 @@ use Inertia\Inertia;
 */
 
 Route::middleware(['guestOrVerified'])->group(function () {
-    Route::get('/', function () {
+    Route::get('/underconstruction', function () {
         return view('underconstruction');
     });
     Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'App\Http\Controllers\LanguageController@switchLang']);
-    Route::get('/welcome',[WelcomeController::class, 'index'])->name('welcome');
+    Route::get('/',[WelcomeController::class, 'index'])->name('welcome');
 
     //Productos 
     Route::get('/all', [ProductController::class, 'index'])->name('products.index');
