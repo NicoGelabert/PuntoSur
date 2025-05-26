606169a876fd29be3d8663c9198c2ea1155b8d5e
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-05-26 12:10:12 +0200

    setup commit

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -17,31 +17,9 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        $homeherobanners = HomeHeroBanner::all();
-        $products = Product::all();
-        $abouts = About::all();
-        $articles = Article::orderBy('created_at', 'desc')->limit(5)->get();
-        // $services = Service::all();
-        // $tags = Tag::all();
-        // $clients = Client::all();
-        // $projects = Project::with('tags', 'clients')->whereHas('services', function($query) {
-        //     $query->where('service_id', 2);
-        // })->get();
-        // $devprojects = Project::with('tags', 'clients')->whereHas('services', function($query) {
-        //     $query->where('service_id', 1);
-        // })->get();
-        // $faqs = Faq::all();
-        return view('welcome', compact(
-            'homeherobanners',
-            'products',
-            'abouts',
-            'articles',
-            // 'services',
-            // 'tags',
-            // 'clients',
-            // 'projects',
-            // 'devprojects',
-            // 'faqs'
-        ));
+        // $products = Product::all();
+        // return view('welcome', compact(
+        //     'products',
+        // ));
     }
 }
