a970f189871a6c245802384566a5b67100ac0c03
Nicolas Gelabert <nicolas.gelabert@takefortytwo.com>
2025-06-02 13:32:42 +0200

    Home changes. History component

--- resources/views/components/home-hero-banner.blade.php
+++ resources/views/components/home-hero-banner.blade.php
@@ -2,18 +2,18 @@
     <div class="w-full absolute top-0 flex h-full">
         <div class="h-full flex flex-col justify-between w-1/2">
             <div class="izquierdo-superior md:h-1/2">
-                <img src="{{ asset('storage/images/common/izquierdo-superior.png') }}" alt="" class="h-full w-auto">
+                <img src="{{ asset('storage/images/common/izquierdo-superior.png') }}" alt="" class="h-auto md:h-full w-auto">
             </div>
             <div class="izquierdo-inferior md:h-1/2">
-                <img src="{{ asset('storage/images/common/izquierdo-inferior.png') }}" alt="" class="h-full w-auto">
+                <img src="{{ asset('storage/images/common/izquierdo-inferior.png') }}" alt="" class="h-auto md:h-full w-auto">
             </div>
         </div>
         <div class="h-full flex flex-col justify-between w-1/2 items-end">
             <div class="derecho-superior md:h-1/2">
-                <img src="{{ asset('storage/images/common/derecho-superior.png') }}" alt="" class="h-full w-auto">
+                <img src="{{ asset('storage/images/common/derecho-superior.png') }}" alt="" class="h-auto md:h-full w-auto">
             </div>
             <div class="derecho-inferior md:h-1/2">
-                <img src="{{ asset('storage/images/common/derecho-inferior.png') }}" alt="" class="h-full w-auto">
+                <img src="{{ asset('storage/images/common/derecho-inferior.png') }}" alt="" class="h-auto md:h-full w-auto">
             </div>
         </div>
     </div>
